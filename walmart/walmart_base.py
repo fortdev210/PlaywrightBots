f8529e4c881894059778a965694c6acdf3ce8733
xiaoping-stlpro <xiaoping@stlpro.com>
2021-08-06 15:34:53 -0400

    add extra item cancel process to walmartbase

--- walmart/walmart_base.py
+++ walmart/walmart_base.py
@@ -48,3 +48,49 @@ class WalmartBase(BotManager):
         self.wait_element_loading('[data-automation-id="signup-submit-btn"]')
         self.click_element('[data-automation-id="signup-submit-btn"]')
     
+    def cancel_extra_item(self, extra_item_number):
+        content = """
+            (extraItemNumber) => {
+                const selector = `[href=\"/ip/${extraItemNumber}\"]`;
+                const parent = document.querySelector([selector]).parentElement
+                    .parentElement.parentElement.parentElement.parentElement;
+                return parent.querySelector('[data-automation-id="shipment-status"]')
+                    .innerText;
+            }, extraItemNumber
+        """
+        extra_item_status = self.page.evaluate(content)
+        LOGGER.info(f'Extra item status is {extra_item_status}')
+        
+        if 'arrives by' in extra_item_status.lower():
+            content = """
+                (extraItemNumber) => {
+                    const selector = `[href=\"/ip/${extraItemNumber}\"]`;
+                    const parent = document.querySelector([selector]).parentElement
+                    .parentElement.parentElement.parentElement.parentElement;
+                    try {
+                    parent
+                    .querySelector('[class="order-details-cancellation"]')
+                    .querySelector("button")
+                    .click();
+                    } catch (error) {
+                    }
+                }, extraItemNumber"""
+            self.page.evaluate(content)
+            self.sleep(3)
+
+            try:
+                self.wait_element_loading('form[class="cancellation-form"]')
+                self.click_element('[name="subReasonCode"]')
+                self.sleep(2)
+                content = """
+                    document
+                    .querySelector('[class="cancellation-form"]')
+                    .querySelector('[type="submit"]')
+                    .click();
+                """
+                LOGGER.info("Successfully cancelled extra item.")
+            except:
+                LOGGER.error("Failed to select the reason.")
+
+        return extra_item_status
+
