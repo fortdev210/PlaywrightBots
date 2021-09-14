fe34d922a5684aacfdbd3dcdcd298a1e1696cff0
xiaoping-stlpro <xiaoping@stlpro.com>
2021-09-14 13:42:12 -0400

    cancel extra item

--- libs/walmart/walmart_cancel_extra_item.py
+++ libs/walmart/walmart_cancel_extra_item.py
@@ -28,9 +28,9 @@ class WalmartCancelExtraItem(WalmartBase):
                 const items = document.querySelectorAll(
                     '[class*="flex-column order-0"]')
                 for (let item of items) {
-                    states.push(item.innerText.replace('\n','').replace('Shipping',''))
+                    states.push(item.innerText.replace('Shipping',''))
                 }
-                return items[0].innerText
+                return states
         }
         """
         item_states = self.page.evaluate(content)
@@ -47,13 +47,38 @@ class WalmartCancelExtraItem(WalmartBase):
         }
         """
         item_numbers = self.page.evaluate(href_content)
-        print(item_numbers)
 
         extra_item_status_text = item_states[item_numbers.index(
-            self.extra_item_number)]
+            self.extra_item_number)].strip()
         main_item_status_text = item_states[item_numbers.index(
-            self.main_item_number)]
-        print(main_item_status_text, extra_item_status_text)
+            self.main_item_number)].strip()
+        LOGGER.info("Extra Item Status: %s", extra_item_status_text)
+
+        if 'Arrives by' in extra_item_status_text:
+            self.wait_element_loading('text=Request cancellation')
+            cancel_btns = self.page.query_selector_all(
+                'text=Request cancellation')
+            cancel_btns[-1].click()
+            self.wait_element_loading('text=Ordered wrong item or amount.')
+            self.click_element('text=Ordered wrong item or amount.')
+            self.sleep(2)
+            self.click_element('[data-testid="panel-cancel-cta"]')
+            self.sleep(2)
+            LOGGER.info('Extra item successfully canceled.')
+
+        if extra_item_status_text.lower() == "canceled":
+            LOGGER.info('Extra item is in canceled state.')
+            self.api.update_extra_item_status(
+                self.order.get('id'),
+                self.order.get('supplier_order_numbers_str'), "success")
+
+            if main_item_status_text.lower() == "canceled":
+                self.api.update_ds_order_status(
+                    self.order.get(
+                        'id'), DropShipOrderStatus.CANCELLATION_REVIEW
+                )
+            LOGGER.info('Successfully updated')
+        self.close_browser()
 
     def run(self):
         self.get_item_numbers()
@@ -72,6 +97,7 @@ class WalmartCancelExtraItem(WalmartBase):
             if current_link == 'https://www.walmart.com/orders':
                 LOGGER.info('New Walmart Page...')
                 self.handle_new_walmart()
+                return
             extra_item_status = self.cancel_extra_item(self.extra_item_number)
             main_item_status = self.get_item_status(self.main_item_number)
             LOGGER.info("Main Item Status: %s", main_item_status)
